4aace5e1562dcf5e9644bb1f461f04b33dd72b9c
RomanFadeev26 <r.fadeew1991@yandex.ru>
2019-10-16 15:41:46 +0300

    Add build folder and mobile behavior

---
+++ build/precache-manifest.b4ba9ff3c5da93834c8f2dad9e4c8165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0866f5888a9c42d5b38cff19bccd69c9",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "fa7d9f227a08135cb4b1",
+    "url": "/kanban-board/static/css/main.640c849f.chunk.css"
+  },
+  {
+    "revision": "d956d87a8be6958c7de9",
+    "url": "/kanban-board/static/js/2.8d3d26b1.chunk.js"
+  },
+  {
+    "revision": "fa7d9f227a08135cb4b1",
+    "url": "/kanban-board/static/js/main.91410cb1.chunk.js"
+  },
+  {
+    "revision": "db9f02281f64c3d2a4f5",
+    "url": "/kanban-board/static/js/runtime-main.09ae0731.js"
+  }
+]);
